dd927e22abb309d87c6ffdbee8ccbf66fdb895eb
david <vermicious.knid.d@gmail.com>
2023-05-27 14:35:19 -0700

    Adds mine endpoint functionality. Adds uuid for random fake addresses.

--- dev/api-common.cjs
+++ dev/api-common.cjs
@@ -1,5 +1,9 @@
 const express = require("express")
 const Blockchain = require("./blockchain-common.cjs")
+const { v1: uuidv1 } = require("uuid");
+
+// Initialize node
+const nodeAddress = uuidv1().split("-").join("")
 
 // Initialize Express
 const app = express()
@@ -23,7 +27,30 @@ app.post("/transaction", function (req, res) {
 
 // Mine
 app.get("/mine", function (req, res) {
-  res.send("mine")
+  // Get previous block
+  const previousBlock = davecoin.getLastBlock()
+  const previousBlockHash = previousBlock["hash"]
+  // Set current block data
+  const currentBlockData = {
+    index: previousBlock + 1,
+    timestamp: Date.now(),
+    transactions: davecoin.pendingTransactions,
+    previousBlockHash: previousBlockHash
+  }
+  // Calculate nonce
+  const nonce = davecoin.proofOfWork(previousBlockHash, currentBlockData)
+  // Hash current block
+  const blockHash = davecoin.hashBlock(previousBlockHash, currentBlockData, nonce)
+
+  // Send reward to block miner
+  davecoin.createNewTransaction(6.45601608, "000000000000000000000000000000000000000000", nodeAddress)
+
+  // Create new block
+  const newBlock = davecoin.createNewBlock(nonce, previousBlockHash, blockHash)
+  res.json({
+    "note": "New block mined successfully.",
+    "block": newBlock
+  })
 })
 
 // Start server
