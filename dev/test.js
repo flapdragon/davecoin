92aebf9757050d12332f55a3f6e986330ccd9d49
david <vermicious.knid.d@gmail.com>
2023-06-06 11:05:35 -0700

    Updates chainIsValid method. Adds tests to test.js that need to be updated. Updates curl tests.

--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,56 @@ import Blockchain from "./blockchain.js"
 
 const davecoin = new Blockchain()
 
-// Test genesis block
-console.log(davecoin)
+// Sample chain from test data  
+const testChain01 = {
+  "chain": [
+    {
+      "index": 1,
+      "timestamp": 1686074541391,
+      "transactions": [],
+      "nonce": 100,
+      "previousBlockHash": "0",
+      "hash": "0"
+    }
+  ],
+  "pendingTransactions": [],
+  "currentNodeUrl": "http://localhost:3001",
+  "networkNodes": []
+}
+console.log("Valid01:", davecoin.chainIsValid(testChain01.chain))
+
+const testChain02 = {
+  "chain": [
+    {
+      "index": 1,
+      "timestamp": 1686074565080,
+      "transactions": [],
+      "nonce": 100,
+      "previousBlockHash": "0",
+      "hash": "0"
+    },
+    {
+      "index": 2,
+      "timestamp": 1686074568578,
+      "transactions": [],
+      "nonce": 2125,
+      "previousBlockHash": "0",
+      "hash": "000059eb65fc162c2544fa84f2512c89cd9a83e023f07c956334f47ee6ac144c"
+    }
+  ],
+  "pendingTransactions": [
+    {
+      "amount": 6.45601608,
+      "sender": "000000000000000000000000000000000000000000",
+      "recipient": "56ab1e70049411ee99d94fe5d7f513ee",
+      "transactionId": "58c32900049411ee99d94fe5d7f513ee"
+    }
+  ],
+  "currentNodeUrl": "http://localhost:3001",
+  "networkNodes": []
+}
+console.log("Valid02:", davecoin.chainIsValid(testChain02.chain))
+
 
 // NOTES: Bitcoin address formats
 // P2PKH/Legacy
